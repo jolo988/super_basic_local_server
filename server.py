dc3e83f1144fbb8d626a06384e24b3bcf0746ac2
Jonathan Lo <126200054+jolo988@users.noreply.github.com>
2023-03-07 21:53:44 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, url_for, request, redirect
+app = Flask(__name__)
+import csv
+
+# basic website route
+@app.route("/")
+def my_home():
+    return render_template('index.html')
+
+# make pages dynamic, so we won't have to copy/paste fixed decorators
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name )
+
+# store email/msg to database.txt (write to FILE)
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email}, {subject}, {message}')
+
+# 'a' append to database.txt
+
+def write_to_csv(data):
+    with open('database.csv', newline="", mode='a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
+# CSV = comma separated values
+# delimiter = will separate into columns
+
+# backend email form, send info to database.txt
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'Something went wrong, try again!'
+
+# POST = browser wants us to save info
+# GET = Browser wants us to get info
